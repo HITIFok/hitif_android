ea5b45b7c5562f1c55c6184c3762c092a235f945
HITIFok <tianahitif@gmail.com>
2026-05-11 12:27:40 +0000

    Fix: DownloadManager.Query sort API compatibility

--- app/src/main/java/com/hitif/videodownloader/ui/DownloadHistoryActivity.kt
+++ app/src/main/java/com/hitif/videodownloader/ui/DownloadHistoryActivity.kt
@@ -102,7 +102,7 @@ class DownloadHistoryActivity : AppCompatActivity() {
 
         val query = DownloadManager.Query()
             .setFilterByStatus(DownloadManager.STATUS_SUCCESSFUL or DownloadManager.STATUS_RUNNING or DownloadManager.STATUS_PENDING or DownloadManager.STATUS_PAUSED)
-            .setOrderBy(DownloadManager.Query.ORDER_DESCENDING)
+            .setSortBy(DownloadManager.Query.SortDirection.DESCENDING, DownloadManager.COLUMN_LAST_MODIFIED_TIMESTAMP)
 
         val cursor: Cursor? = dm.query(query)
 
